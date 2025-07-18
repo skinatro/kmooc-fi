5a5e9ff2b5921e0c06d28110a887b694910864c6
Skinatro <work.omkate@gmail.com>
2025-07-18 20:23:03 +0530

    Added flask functionality and set up ingress for log output accoring to 1.7

--- log_output/log_output.py
+++ log_output/log_output.py
@@ -1,9 +1,30 @@
 # Requirements
 # Print a random string every 5 seconds with timestamp
 
-import datetime, random, string, time
-rstr = ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(32))
-while True:
-    timestamp = datetime.datetime.now()
-    print(f"{timestamp} {rstr}")
-    time.sleep(5)
+import datetime, random, string, time,logging,os,threading
+from flask import Flask , render_template
+
+HASH_STRING = ""
+
+def generate_random_log():
+    global HASH_STRING
+    while True:
+        rstr = ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(32))
+        timestamp = datetime.datetime.now()
+        HASH_STRING = f"{timestamp} {rstr}"
+        print(HASH_STRING)
+        time.sleep(5)
+
+
+logging.getLogger('werkzeug').setLevel(logging.ERROR)
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html',hash_string=HASH_STRING)
+
+if __name__ == "__main__":
+    threading.Thread(target=generate_random_log, daemon=True).start()
+    port = os.environ.get("PORT","5000")
+    print(f"The Server started at {port}")
+    app.run(host="0.0.0.0",port=int(port), debug=True)
