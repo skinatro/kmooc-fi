fd0fb6c7d244c3d62489a9741ee5f8d3896f1ab0
Skinatro <work.omkate@gmail.com>
2025-07-23 00:57:06 +0530

    Exc 2.5

--- apps/log_output/src/log_output_read.py
+++ apps/log_output/src/log_output_read.py
@@ -13,16 +13,24 @@ def index():
     """
     file_path_logs = "/tmp/kube/logs.txt"
     #file_path_pongs = "/tmp/kube/pongs.txt"
-    
+    file_path_confmap = "/etc/config/information.txt"
+
+    with open(file_path_confmap,'r') as confmap:
+        confmapfile_content = confmap.read()
+
+    envvar_content = os.environ.get("MESSAGE")
+
     with open(file_path_logs,'r') as logs:
         logs_content = logs.read()
     # with open(file_path_pongs,'r') as pongs:
     #     pongs_content = pongs.read()
+    
     with requests.get(url="http://ping-pong-app-svc:5000/pings", timeout=3) as response:
         pongs_content = response.text
-    return f"{logs_content} <br> Ping / Pongs: {pongs_content}"
+        
+    return f"file content: {confmapfile_content} <br> env variable: MESSAGE={envvar_content} <br> {logs_content} <br> Ping / Pongs: {pongs_content}"
 
 if __name__ == "__main__":
-    port = os.environ.get("PORT","5000")
+    port = os.environ.get("PORT")
     print(f"The Server started at {port}")
     app.run(host="0.0.0.0",port=int(port), debug=True)
